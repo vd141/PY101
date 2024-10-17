239f0859072b51b45308761d59ec70664f78677d
vd141 <victor.dinh141@gmail.com>
2024-10-16 19:38:51 -0700

    python pip installtion notes

---
+++ Lesson_1/python_pip.py
@@ -0,0 +1,7 @@
+'''
+Installing Pylint
+
+Python3 is installed via homebrew, which means it is externally managed. Trying to install pip packages gives off a warning about creating an virtual environment before installing python packages. Installing a python package in a virtual environment is recommended, as the alternative (installing python packages systemwide) could cause dependency issues.
+
+In short, create a virtual environment for the pip packages, then install the pip packages.
+'''
