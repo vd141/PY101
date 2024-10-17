40d9ce78b66c5f605666c27ea8a4cb9c169b424f
vd141 <victor.dinh141@gmail.com>
2024-10-17 11:00:21 -0700

    completed isnt_it_odd

---
+++ Lesson_1/lint_test.py
@@ -0,0 +1,4 @@
+print('Hello world!')
+
+for letter in 'dinosaur':
+    print(letter)
---
+++ Lesson_2/isnt_it_odd.py
@@ -0,0 +1,8 @@
+# Easy 1: Isn't it Odd?
+
+def is_absolute_value_odd(integer):
+    return abs(integer) % 2 != 0
+
+print(is_absolute_value_odd(7))
+print(is_absolute_value_odd(0))
+print(is_absolute_value_odd(-4))
