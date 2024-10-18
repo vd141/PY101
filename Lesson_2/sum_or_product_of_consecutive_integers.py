8d4a0e60beb095e3e9bbfc6df67355d1d3ade6b9
vd141 <victor.dinh141@gmail.com>
2024-10-17 18:43:36 -0700

    rework (practice) successful'

--- Lesson_2/sum_or_product_of_consecutive_integers.py
+++ Lesson_2/sum_or_product_of_consecutive_integers.py
@@ -1,22 +1,26 @@
 # Sum or Product of Consecutive Integers
 
-upper_limit = int(input('Please enter an integer greater than 0: '))
-operation = input('Enter "s" to compute the sum, or "p" to compute the product. ')
+def product(lower, upper):
+    product = 1
+    for i in range(lower, upper+1):
+        product *= i
+    return product
 
-if upper_limit <= 0:
-    print('Please enter an integer greater than 0.')
-    break
+def sum_(lower, upper):
+    sum = 0
+    for i in range(lower, upper+1):
+        sum += i
+    return sum
 
-match operation:
-    case 's':
-        sum = 0
-        for i in range(1, upper_limit+1):
-            sum += i
-        print(f'The sum of the integers between 1 and {upper_limit} is {sum}')
-    case 'p':
-        product = 1
-        for i in range(1, upper_limit+1):
-            product *= i
-        print(f'The product of the integers between 1 and {upper_limit} is {product}')
-    case _:
-        print('Please enter a valid option: s or p')
+maximum = int(input('Please enter an integer greater than 0: '))
+sum_or_product = input('Enter "s" to compute the sum, or "p" to compute '
+                       'the product. ')
+
+if sum_or_product == 's':
+    print(f'The sum of the integers between 1 and {maximum} is '
+          f'{sum_(1, maximum)}')
+elif sum_or_product == 'p':
+    print(f'The product of the integers between 1 and {maximum} '
+          f'is {product(1,maximum)}')
+else:
+    print('Please enter either p or s ')
