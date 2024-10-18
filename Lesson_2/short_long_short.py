9ef034bd5716ebd72184af313f212654cd86dbcd
vd141 <victor.dinh141@gmail.com>
2024-10-17 18:48:00 -0700

    shortened the code by removing duplicate variable assignments

--- Lesson_2/short_long_short.py
+++ Lesson_2/short_long_short.py
@@ -2,18 +2,14 @@
 
 # Write a functino that takes two strings as arguments, determines the length of
 # the two strings, and then returns the result of concatenating the shorter
-# string, the longer string, and teh shorter string once again. You may assume
+# string, the longer string, and the shorter string once again. You may assume
 # that the strings are of different lengths.
 
 def short_long_short(str1, str2):
     if len(str1) <= len(str2):
-        shorter = str1
-        longer = str2
+        return str2.join([str1, str1,])
     else:
-        longer = str1
-        shorter = str2
-
-    return longer.join([shorter, shorter,])
+        return str1.join([str2, str2, ])
 
 str1 = input('Give me a string. ')
 str2 = input('Give me another string. ')
