2bf193ffa8a66b57328082abddc023feee59fcfe
vd141 <victor.dinh141@gmail.com>
2024-10-20 13:22:03 -0700

    added a function called 'middle' that prints the middle word of each sentence, with error checking'

--- Lesson_2/Easy2/the_end_is_not_here_but_near.py
+++ Lesson_2/Easy2/the_end_is_not_here_but_near.py
@@ -3,22 +3,30 @@
 def penultimate(sentence):
     sentence = sentence.strip(' ')
     if ' ' in sentence:
-        if sentence != ' '*len(sentence):
+        if sentence != ' ' * len(sentence):
             return sentence.split(' ')[-2]
         return ''
     else:
         return sentence
 
-# TODO: Edge cases where there is less than one word or no words
-# return 
-# check if there are spaces, else return an empty string
-# if there are spaces, check if the string is only spaces. if it is, return an
-# string
-# if there is only one word, return that word
-
-
-
 # New function: Return middle word
+# first, strip whitespace, check for edge cases
+# what remains is the sentence. if the sentence has an even number of words, 
+# return the word at the index
+# [0 1 2 3 4] length of 5 midpoint is 2.5 (rounded to 2 with floor division)
+# [0 1 2 3] length of 4 midpoint is 2
+# if length of sentence is 1 word, return that word
+
+def middle(sentence):
+    sentence = sentence.strip(' ')
+    if ' ' in sentence:
+        if sentence != ' ' * len(sentence):
+            sentence_list = sentence.split(' ')
+            num_words = len(sentence_list)
+            return sentence_list[num_words//2]
+        return ''
+    else:
+        return sentence
 
 
 # These examples should print True
@@ -31,9 +39,17 @@ print(penultimate("      ") == "")
 print(penultimate("last     ") == "last")
 print(penultimate("last") == "last")
 
+# a string is passed by value into a python function
 a_word = '     wordy bird'
 print(len(a_word))
 print(penultimate("last     ") == "last")
 print(penultimate(a_word))
 print(a_word)
 print(len(a_word))
+
+print(middle('this is a sentence with odd number of words')) # prints with
+print(middle('even number of words')) # prints of
+print(middle('word')) # prints word
+print(middle('   word   ')) # prints word
+print(middle('')) # prints ''
+print(middle('   ')) # prints ''
