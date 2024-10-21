c44112390494d7e227c888f2a604e37c0abfa03e
vd141 <vd141@users.noreply.github.com>
2024-10-21 22:13:19 +0000

    completed madlibs

---
+++ Lesson_2/Easy3/madlibs.py
@@ -0,0 +1,10 @@
+# Madlibs
+
+noun = input('Enter a noun: ')
+verb = input('Enter a verb: ')
+adjective = input('Enter an adjective: ')
+adverb = input('Enter an adverb: ')
+
+story = f'Do you ever {verb} your {adjective} {noun} {adverb}? That\'s hilarious!'
+
+print(story)
