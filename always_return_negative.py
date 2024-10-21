86b655d846fbba1d3e867bf76db69c4c8f35b5b6
vd141 <vd141@users.noreply.github.com>
2024-10-21 20:01:17 +0000

    completed always return negative

---
+++ always_return_negative.py
@@ -0,0 +1,12 @@
+# Always return negative
+# Write a function that takes a number as an argument. If the argument is a 
+# positive number, return the negative of that number. If the argument is a
+# negative number, return it as-is
+
+def negative(number):    
+    return (number if number <= 0 else -number)
+    # -abs() can also be used
+    
+print(negative(5) == -5)      # True
+print(negative(-3) == -3)     # True
+print(negative(0) == 0)       # True
