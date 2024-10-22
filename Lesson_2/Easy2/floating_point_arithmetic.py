c3ea8f00e80ca255861096a5dfefe2c3cfdb6585
vd141 <vd141@users.noreply.github.com>
2024-10-22 21:27:44 +0000

    encapsulated expressions and calculations in separate functions for modularity

--- Lesson_2/Easy2/floating_point_arithmetic.py
+++ Lesson_2/Easy2/floating_point_arithmetic.py
@@ -7,10 +7,27 @@
 float_1 = float(input('==> Enter the first number: \n'))
 float_2 = float(input('==> Enter the second number: \n'))
 
-print(f'==> {float_1:.6f} + {float_2:.6f} = {(float_1 + float_2):.6f}')
-print(f'==> {float_1:.6f} - {float_2:.6f} = {(float_1 - float_2):.6f}')
-print(f'==> {float_1:.6f} * {float_2:.6f} = {(float_1 * float_2):.6f}')
-print(f'==> {float_1:.6f} / {float_2:.6f} = {(float_1 / float_2):.6f}')
-print(f'==> {float_1:.6f} // {float_2:.6f} = {(float_1 // float_2):.6f}')
-print(f'==> {float_1:.6f} % {float_2:.6f} = {(float_1 % float_2):.6f}')
-print(f'==> {float_1:.6f} ** {float_2:.6f} = {(float_1 ** float_2):.6f}')
+# print(f'==> {float_1:.6f} + {float_2:.6f} = {(float_1 + float_2):.6f}')
+# print(f'==> {float_1:.6f} - {float_2:.6f} = {(float_1 - float_2):.6f}')
+# print(f'==> {float_1:.6f} * {float_2:.6f} = {(float_1 * float_2):.6f}')
+# print(f'==> {float_1:.6f} / {float_2:.6f} = {(float_1 / float_2):.6f}')
+# print(f'==> {float_1:.6f} // {float_2:.6f} = {(float_1 // float_2):.6f}')
+# print(f'==> {float_1:.6f} % {float_2:.6f} = {(float_1 % float_2):.6f}')
+# print(f'==> {float_1:.6f} ** {float_2:.6f} = {(float_1 ** float_2):.6f}')
+
+def expression(float1, float2, operation):
+    return f'==> {float1:.6f} {operation} {float2:.6f} ='
+
+def calculation(float1, float2, operation):
+    match operation:
+        case '+': return float1 + float2
+        case '-': return float1 - float2
+        case '*': return float1 * float2
+        case '/': return float1 / float2
+        case '//': return float1 // float2
+        case '%': return float1 % float2
+        case '**': return float1 ** float2
+
+for operation in ['+', '-', '*', '/', '//', '%', '**']:
+    print(f'{expression(float_1, float_2, operation)} '
+          f'{calculation(float_1, float_2, operation):6f}')
