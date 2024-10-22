2386e3dcde58cdfcec2cd4c8e7214a8b291e2ef3
vd141 <vd141@users.noreply.github.com>
2024-10-22 22:31:15 +0000

    moved always return negative

--- Lesson_2/Easy2/floating_point_arithmetic.py
+++ Lesson_2/Easy2/floating_point_arithmetic.py
@@ -3,17 +3,10 @@
 # then prints the results of the following operations on those two numbers:
 # addition, subtraction, product, quotient, floor quotient, remainder, and 
 # power. Do not worry about validating the input
+# MODULARIZE
 
 float_1 = float(input('==> Enter the first number: \n'))
 float_2 = float(input('==> Enter the second number: \n'))
-
-# print(f'==> {float_1:.6f} + {float_2:.6f} = {(float_1 + float_2):.6f}')
-# print(f'==> {float_1:.6f} - {float_2:.6f} = {(float_1 - float_2):.6f}')
-# print(f'==> {float_1:.6f} * {float_2:.6f} = {(float_1 * float_2):.6f}')
-# print(f'==> {float_1:.6f} / {float_2:.6f} = {(float_1 / float_2):.6f}')
-# print(f'==> {float_1:.6f} // {float_2:.6f} = {(float_1 // float_2):.6f}')
-# print(f'==> {float_1:.6f} % {float_2:.6f} = {(float_1 % float_2):.6f}')
-# print(f'==> {float_1:.6f} ** {float_2:.6f} = {(float_1 ** float_2):.6f}')
 
 def expression(float1, float2, operation):
     return f'==> {float1:.6f} {operation} {float2:.6f} ='
