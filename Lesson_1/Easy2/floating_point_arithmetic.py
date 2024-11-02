e632c76b7c8f411a44fcfb24cf3149f2703d1fea
vd141 <victor.dinh141@gmail.com>
2024-11-02 13:18:24 -0700

    Second run of floating point arithmetic. Encapsulated operations in functions.

--- Lesson_1/Easy2/floating_point_arithmetic.py
+++ Lesson_1/Easy2/floating_point_arithmetic.py
@@ -5,22 +5,61 @@
 # power. Do not worry about validating the input
 # MODULARIZE
 
-float_1 = float(input('==> Enter the first number: \n'))
-float_2 = float(input('==> Enter the second number: \n'))
+# float_1 = float(input('==> Enter the first number: \n'))
+# float_2 = float(input('==> Enter the second number: \n'))
 
-def expression(float1, float2, operation):
-    return f'==> {float1:.6f} {operation} {float2:.6f} ='
+# def expression(float1, float2, operation):
+#     return f'==> {float1:.6f} {operation} {float2:.6f} ='
 
-def calculation(float1, float2, operation):
+# def calculation(float1, float2, operation):
+#     match operation:
+#         case '+': return float1 + float2
+#         case '-': return float1 - float2
+#         case '*': return float1 * float2
+#         case '/': return float1 / float2
+#         case '//': return float1 // float2
+#         case '%': return float1 % float2
+#         case '**': return float1 ** float2
+
+# for operation in ['+', '-', '*', '/', '//', '%', '**']:
+#     print(f'{expression(float_1, float_2, operation)} '
+#           f'{calculation(float_1, float_2, operation):6f}')
+
+def prompt(string):
+    return f'==> {string}'
+
+def calculation(operation, first_number, second_number):
+    first_number = float(first_number)
+    second_number = float(second_number)
+
     match operation:
-        case '+': return float1 + float2
-        case '-': return float1 - float2
-        case '*': return float1 * float2
-        case '/': return float1 / float2
-        case '//': return float1 // float2
-        case '%': return float1 % float2
-        case '**': return float1 ** float2
+            case '+':
+                return first_number + second_number
+            case '-':
+                return first_number - second_number
+            case '*':
+                return first_number * second_number
+            case '/':
+                return first_number / second_number
+            case '//':
+                return first_number // second_number
+            case '%':
+                return first_number % second_number
+            case '**':
+                return first_number ** second_number
 
-for operation in ['+', '-', '*', '/', '//', '%', '**']:
-    print(f'{expression(float_1, float_2, operation)} '
-          f'{calculation(float_1, float_2, operation):6f}')
+welcome_prompt = prompt('Welcome to floating point arithmetic calculator!')
+first_prompt = prompt('Enter the first number:\n')
+second_prompt = prompt('Enter the second number:\n')
+
+print(welcome_prompt)
+first_number = input(first_prompt)
+second_number = input(second_prompt)
+
+operations = ['+', '-', '*', '/', '//', '%', '**']
+
+for operation in operations:
+    result = calculation(operation, first_number, second_number)
+    result_prompt = prompt(f'{first_number} {operation} {second_number} '
+                           f'= {result}')
+    print(result_prompt)
