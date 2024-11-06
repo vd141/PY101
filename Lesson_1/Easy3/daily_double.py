00b624d5ac51e42fa193857d856ff6cf418ec5b9
vd141 <victor.dinh141@gmail.com>
2024-11-05 20:54:17 -0800

    completed daily double

--- Lesson_1/Easy3/daily_double.py
+++ Lesson_1/Easy3/daily_double.py
@@ -4,4 +4,22 @@
 # characters collapsed into a single character
 
 def crunch(string):
-    # one option: split the 
+    # first option: loop through each character of string. save unique character
+    # to a list. join characters of list at the end
+    no_dupes = []
+    prev_char = None
+
+    for i in string:
+        if i != prev_char:
+            prev_char = i
+            no_dupes.append(i)
+
+    return ''.join(no_dupes)
+        
+# These examples should all print True
+print(crunch('ddaaiillyy ddoouubbllee') == 'daily double')
+print(crunch('4444abcabccba') == '4abcabcba')
+print(crunch('ggggggggggggggg') == 'g')
+print(crunch('abc') == 'abc')
+print(crunch('a') == 'a')
+print(crunch('') == '')
