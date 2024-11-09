d14fd488044b95b38bf99c00caa57622523dd666
vd141 <victor.dinh141@gmail.com>
2024-11-09 13:13:05 -0800

    encapsulated code in subfunctions

--- Lesson_2/loan_calculator/loan_calculator.py
+++ Lesson_2/loan_calculator/loan_calculator.py
@@ -14,8 +14,6 @@ ask the user for the APR in percentage points
 ask the user for the loan duration in months
 '''
 
-# TODO: encapsulate code in custom functions
-
 def valid_input(num):
     '''
     Checks if num string can be coerced into a float. If so, checks if float is
@@ -31,29 +29,51 @@ def valid_input(num):
         return False
     return True
 
+def get_loan_dollars():
+    '''
+    Gets loan amount from user and reprompts if input is invalid
+    '''
+    loan_dollars_input = input('==> Please enter the dollar amount of the '
+                         'loan:\n')
+    while not valid_input(loan_dollars_input):
+        loan_dollars_input = input('==> Please enter the dollar amount of the '
+                             'loan:\n')
+    return float(loan_dollars_input)
+
+def get_apr_percent():
+    '''
+    Gets apr percent from user and reprompts if input is invalid
+    '''
+    apr_percent_input = input('==> Please enter the APR of the loan '
+                        '(e.g. 5 for 5%):\n')
+    while not valid_input(apr_percent_input):
+        apr_percent_input = input('==> Please enter the APR of the loan '
+                            '(e.g. 5 for 5%):\n')
+    return float(apr_percent_input)
+
+def get_loan_months():
+    '''
+    gets loan duration in months from user and reprompts if input is invalid
+    '''
+    loan_months_input = input('==> Please enter the duration of the loan in whole '
+                        'months:\n')
+    while not valid_input(loan_months_input):
+        loan_months_input = input('==> Please enter the duration of the loan in '
+                        'whole months:\n')
+    return float(loan_months_input)
+
+
 reuse_calc = True
 print('==> Welcome to Loan Calculator!')
 
 while reuse_calc:
-    loan_dollars = input('==> Please enter the dollar amount of the loan:\n')
-    while not valid_input(loan_dollars):
-        loan_dollars = input('==> Please enter the dollar amount of the loan:\n')
+    loan_dollars = get_loan_dollars()
 
-    apr_percent = input('==> Please enter the APR of the loan (e.g. 5 for 5%):\n')
-    while not valid_input(apr_percent):
-        apr_percent = input('==> Please enter the APR of the loan (e.g. 5 for 5%):\n')
+    apr_percent = get_apr_percent()
 
-    loan_months = input('==> Please enter the duration of the loan in whole '
-                        'months:\n')
-    while not valid_input(loan_months):
-        loan_months = input('==> Please enter the duration of the loan in whole '
-                        'months:\n')
+    loan_months = get_loan_months()
 
-    loan_dollars = float(loan_dollars)
-    apr_percent = float(apr_percent)
-    loan_months = int(loan_months)
-
-    monthly_interest_rate = apr_percent/(12 * 100)
+    monthly_interest_rate = apr_percent / (12 * 100)
 
     monthly_payment = loan_dollars * (monthly_interest_rate /
                                     (1 - (1 + monthly_interest_rate)
