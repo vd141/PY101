f598b241bfca827db8778b732ce1eb7efc4659c7
vd141 <victor.dinh141@gmail.com>
2024-11-08 16:30:37 -0800

    created subfunctions

--- Lesson_1/Easy3/bannerizer.py
+++ Lesson_1/Easy3/bannerizer.py
@@ -1,34 +1,43 @@
-# bannerizer
-# word wrap sentences that are too long to fit
-# number of new strings created is len(original string)/length
-# each new string has length of length or less
+'''
+bannerizer
+word wrap sentences that are too long to fit
+number of new strings created is len(original string)/length
+each new string has length of length or less
+'''
 
+def wrapped_string(string, length):
+    quantity_of_new_strings = abs(-len(string) // length) # ceiling division
+    new_string_list = []
+    lower_index = 0
+
+    for _ in range(quantity_of_new_strings):
+        upper_index = lower_index + length
+        new_string_list.append(string[lower_index:upper_index])
+        lower_index = upper_index
+
+    return new_string_list
+
+def print_header_footer(length):
+    print(f'+-{'-' * length}-+')
+
+def print_whitespace(length):
+    print(f'| {' ' * length} |')
 
 def print_in_box(string, length = float('Inf')):
-
     if len(string) > length:
-        quantity_of_new_strings = abs(-len(string) // length) # ceiling division
-        new_string_list = []
-        lower = 0
-        for i in range(quantity_of_new_strings):
-            upper = lower + length
-            new_string_list.append(string[lower:upper])
-            lower = upper
-    # string = string[:length] # truncate string length
-    string_len = len(string)
-    if len(string) > length:
-        print(f'+-{'-' * length}-+')
-        print(f'| {' ' * length} |')
-        for substring in new_string_list:
+        wrapped_string_as_list = wrapped_string(string, length)
+        print_header_footer(length)
+        print_whitespace(length)
+        for substring in wrapped_string_as_list:
             print(f'| {substring}{(length - len(substring)) * ' '} |')
-        print(f'| {' ' * length} |')
-        print(f'+-{'-' * length}-+')
+        print_whitespace(length)
+        print_header_footer(length)
     else:
-        print(f'+-{'-' * string_len}-+')
-        print(f'| {' ' * string_len} |')
+        print_header_footer(length)
+        print_whitespace(length)
         print(f'| {string} |')
-        print(f'| {' ' * string_len} |')
-        print(f'+-{'-' * string_len}-+')
+        print_whitespace(length)
+        print_header_footer(length)
 
 print_in_box('To boldly go where no one has gone before.', 5)
 print_in_box('')
