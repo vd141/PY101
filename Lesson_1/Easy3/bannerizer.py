d58fc88905626f58c870c849396289e3a6207c62
vd141 <victor.dinh141@gmail.com>
2024-11-08 16:06:29 -0800

    added message wrap feature

--- Lesson_1/Easy3/bannerizer.py
+++ Lesson_1/Easy3/bannerizer.py
@@ -1,15 +1,34 @@
 # bannerizer
+# word wrap sentences that are too long to fit
+# number of new strings created is len(original string)/length
+# each new string has length of length or less
+
 
 def print_in_box(string, length = float('Inf')):
 
     if len(string) > length:
-        string = string[:length]
+        quantity_of_new_strings = abs(-len(string) // length) # ceiling division
+        new_string_list = []
+        lower = 0
+        for i in range(quantity_of_new_strings):
+            upper = lower + length
+            new_string_list.append(string[lower:upper])
+            lower = upper
+    # string = string[:length] # truncate string length
     string_len = len(string)
-    print(f'+-{'-' * string_len}-+')
-    print(f'| {' ' * string_len} |')
-    print(f'| {string} |')
-    print(f'| {' ' * string_len} |')
-    print(f'+-{'-' * string_len}-+')
+    if len(string) > length:
+        print(f'+-{'-' * length}-+')
+        print(f'| {' ' * length} |')
+        for substring in new_string_list:
+            print(f'| {substring}{(length - len(substring)) * ' '} |')
+        print(f'| {' ' * length} |')
+        print(f'+-{'-' * length}-+')
+    else:
+        print(f'+-{'-' * string_len}-+')
+        print(f'| {' ' * string_len} |')
+        print(f'| {string} |')
+        print(f'| {' ' * string_len} |')
+        print(f'+-{'-' * string_len}-+')
 
 print_in_box('To boldly go where no one has gone before.', 5)
 print_in_box('')
