3b88f65e49a07c36f151f634add2835879f6fd08
vd141 <victor.dinh141@gmail.com>
2024-11-13 20:05:39 -0800

    added tracking for best of 5 score

--- Lesson_2/rock_paper_scissors/rock_paper_scissors.py
+++ Lesson_2/rock_paper_scissors/rock_paper_scissors.py
@@ -24,10 +24,15 @@ VALID_CHOICES = {'r': 'rock',
 VALID_CHOICES_SHORTCUTS = list(VALID_CHOICES.keys())
 VALID_CHOICES_VALUES = list(VALID_CHOICES.values())
 
-def determine_winner(player_choice, computer_choice):
+def determine_round_winner(player_choice, computer_choice):
     '''
     Prints a message indicating who won (player or computer) depending on player
     and computer choices
+
+    returns a 1x2 list indicating the winner of that round
+    player wins:   [1, 0]
+    computer wins: [0, 1]
+    tie:           [0, 0]
     '''
     # player win conditions
     if ((player_choice == 'rock' and computer_choice == 'scissors') or
@@ -40,9 +45,10 @@ def determine_winner(player_choice, computer_choice):
         (player_choice == 'spock' and computer_choice == 'rock') or
         (player_choice == 'lizard' and computer_choice == 'paper') or
         (player_choice == 'lizard' and computer_choice == 'spock')):
-        print('You win!')
+        print('==> You win!')
+        return [1, 0]
     # computer win conditions
-    elif ((player_choice == 'rock' and computer_choice == 'spock') or
+    if ((player_choice == 'rock' and computer_choice == 'spock') or
         (player_choice == 'rock' and computer_choice == 'paper') or
         (player_choice == 'paper' and computer_choice == 'lizard') or
         (player_choice == 'paper' and computer_choice == 'scissors') or
@@ -52,11 +58,36 @@ def determine_winner(player_choice, computer_choice):
         (player_choice == 'spock' and computer_choice == 'paper') or
         (player_choice == 'lizard' and computer_choice == 'rock') or
         (player_choice == 'lizard' and computer_choice == 'scissors')):
-        print('Computer wins!')
+        print('==> Computer wins!')
+        return [0, 1]
     # tie
     else:
-        print('It\'s a tie!')
+        print('==> It\'s a tie!')
+        return [0, 0]
 
+def tally_score(result, cumulative_results):
+    '''
+    Updates cumulative result with current result
+    '''
+    cumulative_results[0] += result[0]
+    cumulative_results[1] += result[1]
+
+def determine_game_winner(cumulative_results):
+    '''
+    Prints game results
+    '''
+    str_cumulative_results = list(map(str, cumulative_results))
+    if cumulative_results[0] > cumulative_results[1]:
+        print(f'==> You win the game {cumulative_results[0]}-'
+              f'{cumulative_results[1]}!')
+    elif cumulative_results[1] > cumulative_results[0]:
+        print(f'==> The computer wins the game {cumulative_results[1]}-'
+              f'{cumulative_results[0]}!')
+    else:
+        print(f'==> You and the computer tied '
+              f'{'-'.join(str_cumulative_results)}!')
+
+cumulative_results = [0, 0]
 
 # game
 while True:
@@ -69,24 +100,32 @@ while True:
                               f' {', '.join(VALID_CHOICES_VALUES)}\n'))
         if player_choice in VALID_CHOICES_SHORTCUTS:
             break
-        print('That wasn\'t a valid choice.')
+        print('==> That wasn\'t a valid choice.')
 
     computer_choice = random.choice(VALID_CHOICES_VALUES)
 
     print(f'==> You chose {VALID_CHOICES[player_choice]}, the computer chose'
           f' {computer_choice}.')
 
-    determine_winner(VALID_CHOICES[player_choice], computer_choice)
+    result = determine_round_winner(VALID_CHOICES[player_choice], computer_choice)
+    tally_score(result, cumulative_results)
+
+    if (cumulative_results[0] == 3) or (cumulative_results[1] == 3):
+        determine_game_winner(cumulative_results)
+        break
 
     # user decides whether to continue playing the game or to stop
     while True:
+        print(f'==> The score is currently:\n==> You: {cumulative_results[0]} '
+              f'Computer: {cumulative_results[1]}')
         user_plays_again = input('==> Do you want to play again? y/n\n')
         if user_plays_again.lower() in ['y', 'n']:
             break
-        print('Invalid input. Please enter y/n')
+        print('==> Invalid input. Please enter y/n')
 
     if user_plays_again.lower() == 'n':
-        print('Thanks for playing. Closing program.')
+        determine_game_winner(cumulative_results)
+        print('==> Thanks for playing. Closing program.')
         time.sleep(1.5)
         os.system('clear')
         break
