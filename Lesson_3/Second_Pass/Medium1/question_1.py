447b5622cc88dfd40f528691a3f462efce116644
vd141 <victor.dinh141@gmail.com>
2024-11-27 21:49:17 -0800

    initial commit of second pass medium1 :

---
+++ Lesson_3/Second_Pass/Medium1/question_1.py
@@ -0,0 +1,10 @@
+'''
+write a program that outputs The Flintstones Rock! 10 times, with each line 
+prefixed by one more hyphen than the line above it. The output should start out 
+like this:
+
+-The Flintstones Rock!
+--The Flintstones Rock!
+---The Flintstones Rock!
+    ...
+'''
